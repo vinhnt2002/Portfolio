436421c6b6024deb1d7066c0b2a3e763bb707fa9
vinh <124482049+vinhnt2002@users.noreply.github.com>
2023-12-26 15:57:46 +0700

    fix rabbit

--- portfolio/app/(site)/_components/Header.tsx
+++ portfolio/app/(site)/_components/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <main className=" flex items-center justify-center shadow-2xl ">
       <Wrapper>
-        <div className="lg:w-full md:w-full sm:w-full relative h-[250px] md:h-[200px] overflow-x-hidden">
+        <div className="lg:w-full md:w-full sm:w-full relative h-[250px] md:h-[200px] overflow-hidden">
           <Image
             src="/image/background.jpg"
             alt="image"
@@ -43,7 +43,7 @@ const Header = () => {
         <div className="flex flex-col md:flex-row items-center mt-5 relative top-[-2.75rem] left-0 ">
           <div className="flex-shrink-0 w-40 h-40 relative p-1 bg-white rounded-full ">
             <Image
-              src="/image/avatar.jpg"
+              src="/image/new_avatar.jpg"
               alt="vinhImg"
               fill
               className="rounded-full p-1"
